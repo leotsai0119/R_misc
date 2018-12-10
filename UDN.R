db6a3d2c9879c738585e7e1950b8018587bad1ec
Cai, Yun-Ting <leotsai0119@gmail.com>
2018-12-10 13:24:31 +0800

    added splitting method for titles with a period

--- UDN.R
+++ UDN.R
@@ -12,7 +12,7 @@ rm(list = ls())
 
 #前置作業
 #到UDN網站確認關鍵字搜尋後頁面數p #簡目 #每頁顯示數要選擇50
-keyword <- "((拼經濟OR拚經濟)+資本主義)" #keywords
+#keyword <- "((拼經濟OR拚經濟)+資本主義)" #keywords
 keyword <- "APEC" #keywords
 DateB <- 19500101 #設定開始日期
 DateE <- 20151231 #設定結束日期
@@ -38,88 +38,59 @@ UDNsearch <- function(keyword, DateB, DateE, news, page = 1, sharepage = 50){
 #crawler
 UDNcrawler <- function(){
     
-    pb <- progress_bar$new(
-        format = "處理中 [:bar]:percent | 尚需: :eta | 費時: :elapsed", 
-        total = p, clear = FALSE, width= 80, show_after = 0
-        ) 
+        pb <- progress_bar$new(
+                format = "處理中 [:bar]:percent | 尚需: :eta | 費時: :elapsed", 
+                total = p, clear = FALSE, width= 80, show_after = 0
+                ) 
 
-    start <- Sys.time() #紀錄開始時間
+        start <- Sys.time() #紀錄開始時間
   
-    #list
-    title <- list()
-    date <- list()
+        #list
+        title <- list()
+        date <- list()
   
-    #for.loop
-    for(i in 1:p){
-        url <- UDNsearch(keyword, DateB, DateE, news, page = i)
-        doc <- read_html(url)
-        html_nodes(doc, ".control-pic a") %>% html_text() -> title[[i]]
-        html_nodes(doc, ".news span") %>% html_text() -> date[[i]]
-        pb$tick()
-        Sys.sleep(t)
+        #for.loop
+        for(i in 1:p){
+                url <- UDNsearch(keyword, DateB, DateE, news, page = i)
+                doc <- read_html(url)
+                html_nodes(doc, ".control-pic a") %>% html_text() -> title[[i]]
+                html_nodes(doc, ".news span") %>% html_text() -> date[[i]]
+                pb$tick()
+                Sys.sleep(t)
+                }
+  
+        #unlist
+        title <- unlist(title)
+        date <- unlist(date)
+  
+        #data.frame
+        tstrsplit(date, "．") -> d1 #分割日期、報別、記者
+        tstrsplit(title, ".", fixed = TRUE) -> t1 #分割標號
+        d1[[1]] %>% tstrsplit("-") -> d2 #分割年月日
+  
+        #dataframe 合併年、月、日、標題、報別、記者
+        df <- cbind.data.frame(d2[[1]], d2[[2]], d2[[3]], t1[[2]], d1[[2]], d1[[5]])
+  
+        #作keyword變數
+        df$keyword <- keyword
+  
+        #variable names
+        names(df) <- c("year", "month", "day", "title", "paper", "author", "keyword")
+  
+        #結束
+        end <- Sys.time() #紀錄結束時間
+        print(end - start) #輸出總花費時間
+  
+        return(df)
         }
-  
-    #unlist
-    title <- unlist(title)
-    date <- unlist(date)
-  
-    #data.frame
-    tstrsplit(date, "．") -> d1 #分割日期、報別、記者
-    d1[[1]] %>% tstrsplit("-") -> d2 #分割年月日
-  
-    #dataframe 合併年、月、日、標題、報別、記者
-    df <- cbind.data.frame(d2[[1]], d2[[2]], d2[[3]], title, d1[[2]], d1[[5]])
-  
-    #作keyword變數
-    df$keyword <- keyword
-  
-    #variable names
-    names(df) <- c("year", "month", "day", "title", "paper", "author", "keyword")
-  
-    #結束
-    end <- Sys.time() #紀錄結束時間
-    print(end - start) #輸出總花費時間
-  
-    return(df)
-    }
 
 #shutdown computer
 shutdown <- function(wait = 0){
         Sys.sleep(wait)
         ifelse(.Platform$OS.type == "windows", shell("shutdown -s -t 0"), 
         system("shutdown -h now"))
-}
+        }
 
 
 #test with keyword "APEC"
 df <- UDNcrawler()
-
-######ARCHIVED######
-#前置作業
-#到UDN網站確認關鍵字搜尋後頁面數p #簡目 #每頁顯示數要選擇50
-#keyword <- "((拼經濟OR拚經濟)+資本主義)" #keywords
-
-keyword <- "APEC" #keywords
-DateB <- 19500101 #startdate
-DateE <- 20151231 #設定結束日期
-p <- 2  #設定爬的頁數
-t <- 30 #設定間隔秒數
-#設定完畢
-
-#用這一行檢查能不能正確導引到查詢網址產生結果，可以之後再進loop
-UDNsearch(keyword, DateB, DateE, news = news, page=1) %>% cat()
-
-#執行UDNcrawler 
-labor_97 <- UDNcrawler()
-save(labor_97, file = "D:/labor_97.rda") #存檔路徑用桌面會無法寫入
-
-#關機
-shutdown()
-
-#單一頁面
-url <- UDNsearch(keyword, DateB, DateE, news = news, page=1)
-doc <- read_html(url, encoding = )
-html_nodes(doc, ".control-pic a") %>% html_text() -> title
-html_nodes(doc, ".news span") %>% html_text() -> date
-xml_find_all(doc, xpath = "//a[@class='t']") %>% xml_text() %>% iconv(from = "UTF-8", to = "UTF-8") -> title
-xml_find_all(doc, xpath = "//span[@class='date']") %>% xml_text() %>% iconv(from = "UTF-8", to = "UTF-8") -> date
